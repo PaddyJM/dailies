7474dce8b25ec1092758b70581a42158c80ba3d4
Patrick Morton <patrickjamesmorton@gmail.com>
2024-02-08 08:01:09 +0000

    added a topic with subscription

--- lib/dailies-stack.ts
+++ lib/dailies-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import 'dotenv/config'
 
 export class DailiesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const topic = new sns.Topic(this, 'DailiesTopic', {
+      displayName: 'Dailies Topic',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DailiesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if(!process.env.MY_EMAIL) {
+      throw new Error('Please set the MY_EMAIL environment variable')
+    }
+
+    topic.addSubscription(new EmailSubscription(process.env.MY_EMAIL))
   }
 }
