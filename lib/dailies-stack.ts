dbfaf712765df5c0a7102ab2313bcd74cc54c73c
Patrick Morton <patrickjamesmorton@gmail.com>
2024-02-08 08:59:32 +0000

    another go at changing the cron expression

--- lib/dailies-stack.ts
+++ lib/dailies-stack.ts
@@ -40,7 +40,7 @@ export class DailiesStack extends cdk.Stack {
     reminderFunction.addToRolePolicy(lambdaSnsPolicy);
 
     const reminderEventRule = new events.Rule(this, 'DailiesRemindersRule', {
-      schedule: events.Schedule.expression('cron(* * * * * *)'),
+      schedule: events.Schedule.rate(cdk.Duration.minutes(1)),
       targets: [new eventsTargets.LambdaFunction(reminderFunction)],
     });
 
